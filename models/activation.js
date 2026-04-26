54f18de9295231a381d82e808d93344186ca9d63
ALCM-bit <falcm-bit@outlook.com>
2026-04-26 10:48:48 -0300

    feat: add `activation.findOneValidById()` and `orchestrator.extractUUID()`

--- models/activation.js
+++ models/activation.js
@@ -1,14 +1,15 @@
 import database from "infra/database";
 import email from "infra/email";
+import { NotFoundError } from "infra/erros";
 import webserver from "infra/webserver";
 
 const EXPIRATION_IN_MILISECONDS = 60 * 15 * 1000; // 15 minutes
 
-async function findOneByUserId(userId) {
-  const newToken = await runSelectQuery(userId);
-  return newToken;
+async function findOneValidById(tokenId) {
+  const activationTokenObject = await runSelectQuery(tokenId);
+  return activationTokenObject;
 
-  async function runSelectQuery(userId) {
+  async function runSelectQuery(tokenId) {
     const results = await database.query({
       text: `
         SELECT 
@@ -16,13 +17,22 @@ async function findOneByUserId(userId) {
         FROM
           user_activation_token
         WHERE
-          user_id = $1
+          id = $1
+          AND used_at IS NULL
+          AND expires_at > NOW()
         LIMIT
           1
       ;`,
-      values: [userId],
+      values: [tokenId],
     });
 
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message:
+          "O token de ativação não foi encontrado no sistema ou expirou.",
+        action: "Faça um novo cadastro.",
+      });
+    }
     return results.rows[0];
   }
 }
@@ -66,7 +76,7 @@ Equipe FinTab`,
 const activation = {
   sendEmailToUser,
   create,
-  findOneByUserId,
+  findOneValidById,
 };
 
 export default activation;
